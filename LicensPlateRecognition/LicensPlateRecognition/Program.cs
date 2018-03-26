fca354129cbcba8cbfe14cc8769f37a9428bcb20
Christian Leist <chr.leist@web.de>
2018-03-26 16:02:47 +0200

    Create project LicensePlateRecognition in VS2017

---
+++ LicensPlateRecognition/LicensPlateRecognition/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LicensPlateRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Press any key to proceed..");
+            Console.ReadKey();
+        }
+    }
+}
