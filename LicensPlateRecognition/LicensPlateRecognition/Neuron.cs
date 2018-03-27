d6c3e05c0da3bd6cae63f1af28a6f0b379f60428
Christian Leist <christian.leist@elotec-fischer.de>
2018-03-27 12:19:54 +0200

    added bias in Neuron

--- LicensPlateRecognition/LicensPlateRecognition/Neuron.cs
+++ LicensPlateRecognition/LicensPlateRecognition/Neuron.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LicensPlateRecognition
 {
     public class Neuron
     {
         private double weight;
+        private double bias;
 
         public double Weight
         {
             get { return this.weight; }
             set { this.weight = value; }
+        }
+
+        public double Bias 
+        {
+            get {return this.bias; }
+            set { this.bias = value; }
         }
     }
 }
