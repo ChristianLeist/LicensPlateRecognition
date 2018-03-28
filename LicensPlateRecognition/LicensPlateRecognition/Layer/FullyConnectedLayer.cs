652ce9283c5efa2506923fe360f695f2e3e9ced1
Christian Leist <christian.leist@elotec-fischer.de>
2018-03-28 16:13:21 +0200

    added Layer classes, deleted class Neuron, added class NeuralNetwork

---
+++ LicensPlateRecognition/LicensPlateRecognition/Layer/FullyConnectedLayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LicensPlateRecognition.Layer
+{
+    class FullyConnectedLayer
+    {
+        private double[,] weightMat;
+        private double[,] biasMat;
+
+        public FullyConnectedLayer(int row, int column)
+        {
+            // column = number of neurons in next layer
+            // row = number of neurons in this layer
+            weightMat = new double[row, column];
+            biasMat = new double[row, column];
+        }
+    }
+}
---
+++ LicensPlateRecognition/LicensPlateRecognition/Network/NeuralNetwork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LicensPlateRecognition.Network
+{
+    class NeuralNetwork
+    {
+        private ArrayList layerList = new ArrayList();
+
+        public NeuralNetwork()
+        {
+            // TODO: Create Network with different layers
+        }
+
+        public void StoreNeuralNetwork()
+        {
+            // TODO: Save weights, biases, etc..
+        }
+
+        public void LoadNeuralNetwork()
+        {
+            // TODO: Load weights, biases, etc..
+        }
+    }
+}
